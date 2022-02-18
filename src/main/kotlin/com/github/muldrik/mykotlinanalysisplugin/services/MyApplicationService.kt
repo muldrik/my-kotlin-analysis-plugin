670476d2b081d64caedaabdd647738c73bba5fb7
GitHub Action <action@github.com>
2022-02-18 00:04:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/muldrik/mykotlinanalysisplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.muldrik.mykotlinanalysisplugin.services
+
+import com.github.muldrik.mykotlinanalysisplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
